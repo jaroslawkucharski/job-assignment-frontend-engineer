e9e0ce45f23552002617ee8f7f5cc5a48eaeb18b
Jarosław <jaroslaw.kucharski@landingi.com>
2026-08-14 15:45:10 +0200

    feat(article): implement article page

--- src/articles/components/ArticleList.tsx
+++ src/articles/components/ArticleList.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 
-import { Article, favoriteArticle, formatArticleDate, getArticles, getAuthorImage, unfavoriteArticle } from "../api/articles";
-import { useAuthentication } from "../authentication";
+import { Article, favoriteArticle, formatArticleDate, getArticles, getAuthorImage, unfavoriteArticle } from "../../api/articles";
+import { useAuthentication } from "../../authentication";
 
 const ArticleList = () => {
   const history = useHistory();
--- src/articles/index.ts
+++ src/articles/index.ts
@@ -1,2 +1,2 @@
-export { default as Article } from "./Article";
-export { default as ArticleList } from "./ArticleList";
+export { default as Article } from "./components/Article";
+export { default as ArticleList } from "./components/ArticleList";
